534e12d02a07335ecaa82570d8ee12d7d0faaf8d
Ofelia <oer.rodriguez@gmail.com>
2021-03-19 04:06:08 +0000

    Created student form and rendered student info

--- hackbright_web.py
+++ hackbright_web.py
@@ -21,6 +21,31 @@ def get_student():
                            github=github)
     return html
 
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+@app.route("/student-add", methods=['POST'])
+def student_add():
+    """Passes data from form submission"""
+
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    github = request.form.get('github')
+
+    return render_template("student_added.html", 
+                            first_name=first_name,
+                            last_name=last_name,
+                            github=github)
+
+@app.route("/add-new-student")
+def student_form():
+    """Renders student-added page """
+
+    return render_template("add_new_student.html")
+
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
     app.run(debug=True, host="0.0.0.0")
